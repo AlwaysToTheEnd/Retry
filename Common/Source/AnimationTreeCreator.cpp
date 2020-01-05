8f21a16a22d6b4b12cc83aaccde026d7a48297eb
경호 최 <rudgh710@naver.com>
2020-01-06 02:41:30 +0900

    ..

--- Common/Source/AnimationTreeCreator.cpp
+++ Common/Source/AnimationTreeCreator.cpp
@@ -643,10 +643,6 @@ void VisualizedAniTreeCreator::DeleteNode(AniNodeVisual* node)
 #include <Windows.h>
 #include <ShlObj.h>
 
-#pragma comment(lib, "comctl32.lib")
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' "\
-                       "version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 void VisualizedAniTreeCreator::TestCode()
 {
 	IFileOpenDialog* pfsd;
